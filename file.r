381636815276b80e6ad317aff61472d23897d4e1
bethmccloskey <bethmc@live.co.uk>
2026-01-16 16:41:44 +0000

    Update file.r

--- file.r
+++ file.r
@@ -1,8 +1,5 @@
 library(tidyverse)
 
-hello <- 4
-
-
 fev1 <- read.csv('fev1.csv')
 
 set.seed(10)
@@ -13,3 +10,10 @@ fev1_sampled <- fev1 %>%
   select(id) %>% 
   inner_join(fev1)
 
+## 3c: FEV, Age, and Height
+
+ggplot(fev1_sampled, aes(x = age, y=FEV1, fill = height, colour = height)) +
+  geom_point() +
+  labs(title = "Correlating age by FEV and height",
+       x = "Age", 
+       y = "Fev")
